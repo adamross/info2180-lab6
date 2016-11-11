eafe511143ec6183679fd489c7a2c74ac80d6c68
Adam Findlator <afindlator@gmail.com>
2016-11-11 14:30:44 +0000

    commit1

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+ var httpRequest;
+  document.getElementById("searchbttn").onclick = function() { makeRequest('request.php?q=definition'); };
+
+  function makeRequest(url) {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } 
+      else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+}
